18b598a63950f95a74791201911c836676245703
navys00 <73278416+navys00@users.noreply.github.com>
2023-09-23 22:55:40 +0300

    work build without lab

---
+++ lab1/Server/AppServerMain.cpp
@@ -0,0 +1,10 @@
+#include "AppServer.h"
+#include "windows.h"
+
+int main (int argc, char *argv[])
+{
+    int port = argc > 1 ? atoi(argv[1]) : 0;
+    Server s;
+    s.init(port);
+    s.run();
+}
